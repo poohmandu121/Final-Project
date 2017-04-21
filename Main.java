70d716e8f37098cba8b7f84d30fa7d2ebe188d2f
poohmandu121 <finalfantasy12101@gmail.com>
2017-04-20 20:03:58 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+   int firstNumber = 10;
+   int secondNumber = 20;
+
+        System.out.println(firstNumber+secondNumber);
+        System.out.println(firstNumber-secondNumber);
+        System.out.println(firstNumber*secondNumber);
+        System.out.println(firstNumber/secondNumber);
+        System.out.println(firstNumber%secondNumber);
+        System.out.println(++firstNumber);
+        System.out.println(--secondNumber);
+
+        //Part 2
+        System.out.println(firstNumber==secondNumber);
+        System.out.println(firstNumber!=secondNumber);
+        System.out.println(firstNumber>secondNumber);
+        System.out.println(firstNumber<secondNumber);
+        System.out.println(firstNumber<=secondNumber);
+        System.out.println(firstNumber>=secondNumber);
+
+    }
+}
